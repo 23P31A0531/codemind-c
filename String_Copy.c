59701d3676bd4eebc7a4d5d12964fc4400599daa
23P31A0531 <147791845+23P31A0531@users.noreply.github.com>
2023-12-23 11:10:20 +0530

    #include<stdio.h> int main() {     char s1[20],s2[20];     scanf("%[^ ]s",s1);     int i;     for(i=0;s1[i]!=NULL;i++)     {         s2[i]=s1[i];     }     s2[i]=NULL;     printf("%s",s2); }

--- String_Copy.c
+++ String_Copy.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-	char str[20],str1[20];
-	scanf("%[^
-]s",str);
-	strcpy(str1,str);
-	printf("%s",str1);
+    char s1[20],s2[20];
+    scanf("%[^
+]s",s1);
+    int i;
+    for(i=0;s1[i]!=NULL;i++)
+    {
+        s2[i]=s1[i];
+    }
+    s2[i]=NULL;
+    printf("%s",s2);
 }
