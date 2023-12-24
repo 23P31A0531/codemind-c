48f9768ac6028f5b9819db66bff269dc6826a1b1
23P31A0531 <147791845+23P31A0531@users.noreply.github.com>
2023-12-24 22:24:06 +0530

    #include<stdio.h> int main() { 	int i,n,flag=0; 	scanf("%d",&n); 	int arr[n];     for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int se; 	scanf("%d",&se); 	for(i=0;i<n;i++) 	{ 		if(se==arr[i]) 		{ 			flag=1; 			break; 		} 	} 	if(flag==1) 	{ 	    printf("True"); 	} 	else 	{ 	    printf("False"); 	} }

--- Element_search_in_an_Array.c
+++ Element_search_in_an_Array.c
@@ -1,30 +1,29 @@
 #include<stdio.h>
-#include<limits.h>
 int main()
 {
-    int n,i,flag;
-    scanf("%d",&n);
-    int a[n];
+	int i,n,flag=0;
+	scanf("%d",&n);
+	int arr[n];
     for(i=0;i<n;i++)
-    {
-    scanf("%d",&a[i]);
-    }
-    int se;
-    scanf("%d",&se);
-    for(i=0;i<n;i++)
-    {
-        if(se==a[i])
-        {
-            flag=1;
-            break;
-        }
-    }
- if(flag==1)
- {
-     printf("True");
- }
- else
- {
-     printf("False");
- }
+	{
+		scanf("%d",&arr[i]);
+	}
+	int se;
+	scanf("%d",&se);
+	for(i=0;i<n;i++)
+	{
+		if(se==arr[i])
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+	    printf("True");
+	}
+	else
+	{
+	    printf("False");
+	}
 }
