3d484183a7776a132bcf969ae7ab4ac7fcfb413b
23P31A0531 <147791845+23P31A0531@users.noreply.github.com>
2024-01-02 22:14:08 +0530

    #include<stdio.h> int main() { 	int n,count; 	scanf("%d",&n); 	int a[n],i,k; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	for(i=0;i<n;i++) 	{        if(a[i]%2==1)        {        	count=a[i]; 	   } 	} 	k=count; 	printf("%d ",k); }

--- Last_odd_number_in_an_Array.c
+++ Last_odd_number_in_an_Array.c
@@ -1,30 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
-    scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    i=0;
-    int j=n-1,t;
-    while(i<=j)
-    {
-        t=a[i];
-        a[i]=a[j];
-        a[j]=t;
-        i++;
-        j--;
-    }
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2==1)
-        {
-            printf("%d",a[i]);
-            break;
-        }
-    }
-    
+	int n,count;
+	scanf("%d",&n);
+	int a[n],i,k;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+       if(a[i]%2==1)
+       {
+       	count=a[i];
+	   }
+	}
+	k=count;
+	printf("%d ",k);
 }
+	
