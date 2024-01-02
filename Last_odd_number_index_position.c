b423c780d6944fb5fd08ca49898896f106ee7b78
23P31A0531 <147791845+23P31A0531@users.noreply.github.com>
2024-01-02 21:59:03 +0530

    #include<stdio.h> int main() { 	int n,count; 	scanf("%d",&n); 	int a[n],i,k; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	for(i=0;i<n;i++) 	{        if(a[i]%2==1)        {        	count=i; 	   } 	} 	k=count; 	printf("%d ",k); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n,count;
+	scanf("%d",&n);
+	int a[n],i,k;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+       if(a[i]%2==1)
+       {
+       	count=i;
+	   }
+	}
+	k=count;
+	printf("%d ",k);
+}	
