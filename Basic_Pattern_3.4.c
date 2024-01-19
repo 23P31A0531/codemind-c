d9b1cd8b840bd9e9ea2a9e617a197ee285d42f67
23P31A0531 <147791845+23P31A0531@users.noreply.github.com>
2024-01-19 12:11:27 +0530

    #include<stdio.h> int main() {     int i,j,a;     scanf("%d",&a);     for(i=64+a;i>=65;i--)     {         for(j=64+a;j>=65;j--)         {             if(i>=j)             {             printf("%c ",i);             }         }         printf(" ");     }
    }

---
+++ Basic_Pattern_3.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a;
+    scanf("%d",&a);
+    for(i=64+a;i>=65;i--)
+    {
+        for(j=64+a;j>=65;j--)
+        {
+            if(i>=j)
+            {
+            printf("%c ",i);
+            }
+        }
+        printf("
+");
+    }
+    
+}
